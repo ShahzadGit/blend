9f7494304c7edcc7e888b378fe0400417654f5a9
Shahzad Ahmed <shahzad.btm@gmail.com>
2023-05-22 10:14:18 +0500

    Updated Index.html and PowerinG

--- src/components/IntroHead.js
+++ src/components/IntroHead.js
@@ -10,7 +10,7 @@ export default function IntroHead() {
     return (
         <div className="main">
             <div className="left">
-                <h2 style={{ textDecoration: 'none', color: 'pink' }}>Powering</h2>
+                <h2 style={{ textDecoration: 'none', color: 'pink' }}>PowerinG</h2>
                 <h2>deeper banking<br />relationships</h2>
                 <p>Blend is the cloud banking infrastructure powering billions of dollars in financial transactions everyday.</p>
                 <Button variant="outline-success" style={{ backgroundColor: 'white', color: 'darkblue', padding: '10px', marginRight: '10px', border: '1px solid darkblue', width: '200px' }}>See how it works  <FontAwesomeIcon icon={faCirclePlay} /></Button>
